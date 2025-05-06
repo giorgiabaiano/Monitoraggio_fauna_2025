bfe97353e5a4da6d247ca11659f5687780e3a754
giorgiabaiano <giorgiabaiano01@gmail.com>
2025-05-06 12:43:39 +0200

    Update First_code.R

--- First_code.R
+++ First_code.R
@@ -4,3 +4,14 @@ library(terra)
 setwd("C://Users/gdemo/Desktop/")
 mato= rast("Matogrosso_l5_1992219_lrg.jpg")
 plot(mato)
+mato= flip(mato)
+plot(mato)
+
+#Band:
+#band 1 = near infrared
+#band 2 = red
+#band 3 = green
+
+plotRGB(mato, r=2, g=3, b=1) #tutto quello che diventa blu che corrisponede alla banda 1 del vicino infrarosso, che viene riflesso molto dalle piante, equivale alla vegetazione
+
+
